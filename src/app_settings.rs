64cbc55d1427ad22a5a00c4063d5bfe7e9494896
Robert Monden <robert.monden@iodigital.com>
2026-04-10 10:03:27 +0200

    Build-time env

--- src/app_settings.rs
+++ src/app_settings.rs
@@ -3,7 +3,6 @@ use anyhow::{Context, Result};
 #[derive(Debug, Clone)]
 pub struct AppConfig {
     pub azure_api_key: String,
-    pub azure_api_region: String,
     pub azure_api_endpoint: String,
     pub azure_api_version: String,
     pub azure_api_deployment: String,
@@ -11,29 +10,16 @@ pub struct AppConfig {
 
 impl AppConfig {
     pub fn load() -> Result<Self> {
-        let api_key = std::env::var("AR_TRANSLATE_AZURE_API_KEY").context(
-            "Failed to load Azure API key from environment variable 'AR_TRANSLATE_AZURE_API_KEY'",
-        )?;
-
-        let api_region =
-            std::env::var("AR_TRANSLATE_AZURE_API_REGION").unwrap_or("westeurope".to_string());
-
-        let api_endpoint = std::env::var("AR_TRANSLATE_AZURE_API_ENDPOINT")
-            .context("Failed to load Azure API key from environment variable 'AR_TRANSLATE_AZURE_API_ENDPOINT'")?;
-
-        let api_version = std::env::var("AR_TRANSLATE_AZURE_API_VERSION").context(
-            "Failed to load API version from environment variable 'AR_TRANSLATE_AZURE_API_VERSION'",
-        )?;
-
-        let deployment = std::env::var("AR_TRANSLATE_AZURE_API_DEPLOYMENT_NAME")
-            .context("Failed to load deployment name from environment variable 'AR_TRANSLATE_AZURE_API_DEPLOYMENT_NAME'")?;
+        let api_key = option_env!("AR_TRANSLATE_AZURE_API_KEY").unwrap_or("unknown");
+        let api_endpoint = option_env!("AR_TRANSLATE_AZURE_API_ENDPOINT").unwrap_or("unknown");
+        let api_version = option_env!("AR_TRANSLATE_AZURE_API_VERSION").unwrap_or("unknown");
+        let deployment = option_env!("AR_TRANSLATE_AZURE_API_DEPLOYMENT_NAME").unwrap_or("unknown");
 
         Ok(Self {
-            azure_api_key: api_key,
-            azure_api_region: api_region,
-            azure_api_endpoint: api_endpoint,
-            azure_api_version: api_version,
-            azure_api_deployment: deployment,
+            azure_api_key: api_key.to_string(),
+            azure_api_endpoint: api_endpoint.to_string(),
+            azure_api_version: api_version.to_string(),
+            azure_api_deployment: deployment.to_string(),
         })
     }
 }
